01de49b6bd037e4efeca3a8920d1f2d3f6fb3efe
gj <gujian19971006@163.com>
2020-12-08 16:30:11 +0800

    fjfjf

--- test_summary.py
+++ test_summary.py
@@ -23,17 +23,43 @@ import pickle
 import utils
 from torchsummaryX import summary
 from archs.cifar100 import AlexNet, fc1, LeNet5, vgg, resnet
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = fc1.fc1().to(device)
-x = torch.randn(1,3,32,32).to(device)
 
-summary(model,x)
-print(model.state_dict())
+def train(model, train_loader, optimizer, criterion):
+    EPS = 1e-6
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.train()
+    for batch_idx, (imgs, targets) in enumerate(train_loader):
+        optimizer.zero_grad()
+        #imgs, targets = next(train_loader)
+        imgs, targets = imgs.to(device), targets.to(device)
+        output = model(imgs)
+        train_loss = criterion(output, targets)
+        train_loss.backward()
+
+        # Freezing Pruned weights by making their gradients Zero
+        for name, p in model.named_parameters():
+            if 'weight' in name:
+                tensor = p.data.cpu().numpy()
+                grad_tensor = p.grad.data.cpu().numpy()
+                grad_tensor = np.where(tensor < EPS, 0, grad_tensor)
+                p.grad.data = torch.from_numpy(grad_tensor).to(device)
+        optimizer.step()
+    return train_loss.item()
+
+batch_size =10
+transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.1307,), (0.3081,))])
+traindataset = datasets.MNIST('../data', train=True, download=True,transform=transform)
+train_loader = torch.utils.data.DataLoader(traindataset, batch_size=batch_size, shuffle=True, num_workers=0,drop_last=False)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+criterion = nn.CrossEntropyLoss() # Default was F.nll_loss
+
+
 
 model_file = "/data/Lottery-Ticket-Hypothesis-in-Pytorch/saves/fc1/mnist/1_model_lt.pth.tar"
-model_parameters = torch.load(model_file)
-print(model_parameters)
+model = torch.load(model_file)
+optimizer = torch.optim.Adam(model.parameters(), lr=1.2e-3, weight_decay=1e-4)
+loss = train(model, train_loader, optimizer, criterion)
 
-model.load_state_dict(model_parameters)
 x = torch.randn(1,3,32,32).to(device)
 summary(model,x)
